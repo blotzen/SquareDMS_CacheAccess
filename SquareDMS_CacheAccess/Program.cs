751138bc7b3b44fe594348150617488f320586fe
franzmaximilianbrandl <franzmaximilianbrandl@gmail.com>
2020-09-07 09:47:14 +0200

    Projektdateien hinzufügen.

---
+++ SquareDMS_CacheAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SquareDMS_CacheAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
