6fd3477d4ba68a12d0d6f9a0faa295b72f7ec298
franzmaximilianbrandl <franzmaximilianbrandl@gmail.com>
2020-09-07 14:29:50 +0200

    Implemented the Put, Retrieve, Connect and Disconnect Methods for the redis Cache.

--- SquareDMS_CacheAccess/Program.cs
+++ SquareDMS_CacheAccess/Program.cs
@@ -2,11 +2,30 @@
 
 namespace SquareDMS_CacheAccess
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static async System.Threading.Tasks.Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var file = System.IO.File.ReadAllBytes(@"C:\Users\Administrator\Desktop\CompSci.pdf");
+
+            var redis = new SquareCacheRedis("localhost", 6_379);
+
+            await redis.ConnectAsync();
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            await redis.PutDocumentPayloadAsync(1, file);
+
+            sw.Stop();
+
+            await redis.DisconnectAsync();
+
+            Console.WriteLine($"Put in : {sw.ElapsedMilliseconds} ms");
+
+            Console.ReadLine();
         }
     }
 }
